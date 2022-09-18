a04f94a385ae744d78f81252ae00d2e81161d79d
trevordavies095 <trevordavies095@gmail.com>
2022-09-18 15:10:44 -0400

    Clean up files

--- src/Musium.py
+++ src/Musium.py
@@ -1,8 +1,7 @@
 from db_layer import DbLayer
 from math import floor
 from music_brainz import MusicBrainz
-from query_parse import parse_query
-from reporting import output_report
+from prettytable import PrettyTable
 import argparse
 import yaml
 
@@ -37,6 +36,9 @@ def main():
             print("Found album in DB!")
             r = mb.Search(artist, album, year, r[0])
     correct = check_details(r)
+
+    if args.mb_id and correct != "c":
+        exit(0)        
 
     if correct != "c":
         mb_id = input("Enter ID: ")
@@ -112,6 +114,106 @@ def load_config():
             exit(1)
 
         return config
+
+
+def parse_query(args):
+
+    # IF artist and album
+    if args.artist and args.album:
+        s_artist_album_sql = '''
+            SELECT ar.name, al.name, al.year, al.rating, al.star_rating, t.name, t.track_score FROM track t
+            INNER JOIN album al ON al.id = t.album_id 
+            INNER JOIN artist ar ON ar.id = al.artist_id
+            WHERE ar.name = "{}" COLLATE NOCASE
+            AND al.name = "{}" COLLATE NOCASE
+            ORDER BY t.id ASC;
+        '''
+        return ["artist_album", s_artist_album_sql.format(args.artist, args.album)]
+
+    # IF artist
+    if args.artist:
+        s_artist_sql = '''
+            SELECT ar.name, al.name, al.year, al.rating, al.star_rating FROM album al
+            INNER JOIN artist ar ON ar.id = al.artist_id
+            WHERE ar.name = "{}" COLLATE NOCASE
+            ORDER BY al.year ASC;
+        '''
+        return ["artist", s_artist_sql.format(args.artist)]
+
+    # IF year
+    if args.year:
+        s_year_sql = '''
+            SELECT ar.name, al.name, al.year, al.rating, al.star_rating FROM album al
+            INNER JOIN artist ar ON ar.id = al.artist_id
+            WHERE al.year = {}
+            ORDER BY al.rating DESC;
+        '''
+        return ["year", s_year_sql.format(args.year)]
+
+    # IF decade
+    if args.decade:
+        low = int(args.decade.split("s")[0])
+        high = low + 9
+
+        s_decade_sql = '''
+            SELECT ar.name, al.name, al.year, al.rating, al.star_rating FROM album al
+            INNER JOIN artist ar ON ar.id = al.artist_id
+            WHERE al.year BETWEEN {} AND {}
+            ORDER BY al.rating DESC;
+        '''
+        
+        return ["decade", s_decade_sql.format(str(low), str(high))]
+
+    if args.all_time:
+        s_all_time_sql = '''
+            SELECT ar.name, al.name, al.year, al.rating, al.star_rating FROM album al
+            INNER JOIN artist ar ON ar.id = al.artist_id
+            ORDER BY al.rating DESC;
+        '''
+
+        return["all_time", s_all_time_sql]
+
+
+def output_report(r):
+    artist_album_heading = ["Track", "Rating"]
+    generic_heading = ["Artist", "Album", "Year", "Rating", "Stars"]
+
+    report_type = r[0]
+    report = r[1]
+
+    if report_type == "artist_album": 
+        print(report[0][0] + " - " + report[0][1] + " (" + str(report[0][2]) + ")")
+        output_table = PrettyTable(artist_album_heading)
+        for row in report:
+            output_table.add_row([row[5], row[6]])
+        print(output_table)
+        print("Rating: {}, {} stars".format(report[0][3], report[0][4]))
+    else:
+        output_table = PrettyTable(generic_heading)
+        output_table.add_rows(report)
+        print(output_table)
+
+
+
+class ReleaseObj(object):
+    musicbrainz_release_id = ""
+    artist = ""
+    album = ""
+    year = ""
+    track_list = []                         # i0 - track number, i1 - track name, i2 - track score
+    rating = 0
+    star_rating = 0
+
+    def __init__(self, musicbrainz_release_id, artist, album, year, track_list):
+        self.musicbrainz_release_id = musicbrainz_release_id
+        self.artist = artist
+        self.album = album
+        self.year = year
+        self.track_list = track_list
+
+
+    def __str__(self):
+        return "\n".join((self.musicbrainz_release_id, self.artist, self.album, str(self.year)))
 
 
 if __name__ == "__main__":
--- src/music_brainz.py
+++ src/music_brainz.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-from release_obj import ReleaseObj
 import musicbrainzngs as mb
 
 class MusicBrainz:
@@ -56,4 +55,25 @@ class MusicBrainz:
                 track_list.append([line["number"], line["recording"]["title"], -1])
         
         return track_list
+
+
+class ReleaseObj(object):
+    musicbrainz_release_id = ""
+    artist = ""
+    album = ""
+    year = ""
+    track_list = []                         # i0 - track number, i1 - track name, i2 - track score
+    rating = 0
+    star_rating = 0
+
+    def __init__(self, musicbrainz_release_id, artist, album, year, track_list):
+        self.musicbrainz_release_id = musicbrainz_release_id
+        self.artist = artist
+        self.album = album
+        self.year = year
+        self.track_list = track_list
+
+
+    def __str__(self):
+        return "\n".join((self.musicbrainz_release_id, self.artist, self.album, str(self.year)))
 
