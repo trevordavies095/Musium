trevordavies095/Musium

commit 9c20663b1a61530775b1edbf47859e730ad25a23

Fixed division by zero bug

src/musium.py
@@ -1,9 +1,11 @@
+import argparse
+import logging
+import yaml
 from db_layer import DbLayer
 from math import floor
 from music_brainz import MusicBrainz
 from prettytable import PrettyTable
-import argparse
-import yaml
+
 
 def main():
     config = load_config()
@@ -27,13 +29,16 @@ def main():
         if args.artist:
             for r in results:
                 score += r[3]
-            score /= len(results)
+            if len(results) > 0:
+                score /= len(results)
         
-        if args.top_artists:
-            output_report([q[0], results, args.top_artists])
-        else:
-            output_report([q[0], results, score])
-        exit(0)
+                if args.top_artists:
+                    output_report([q[0], results, args.top_artists])
+                else:
+                    output_report([q[0], results, score])
+            else:
+                print("Not found!")
+            exit(0)
 
     # Check to see if musicbrainz ID was passed in
     if args.mb_id:
@@ -130,7 +135,7 @@ def term_args():
 def load_config():
         try:
             stream = open("config.yaml", "r")
-            config = yaml.load(stream)
+            config = yaml.safe_load(stream)
 
         except FileNotFoundError:
             print("Missing config.yaml!")

commit 588fdd5a1d4f239ffe5b9b5dd418135b34f30471

fixed reporting bug introduced in 9c20663

src/musium.py
@@ -31,14 +31,15 @@ def main():
                 score += r[3]
             if len(results) > 0:
                 score /= len(results)
-        
-                if args.top_artists:
-                    output_report([q[0], results, args.top_artists])
-                else:
-                    output_report([q[0], results, score])
             else:
                 print("Not found!")
-            exit(0)
+        
+        if args.top_artists:
+            output_report([q[0], results, args.top_artists])
+        else:
+            output_report([q[0], results, score])
+            
+        exit(0)
 
     # Check to see if exporting csv
     if args.export_csv: